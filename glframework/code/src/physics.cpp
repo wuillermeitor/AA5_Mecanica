187002b7b407c9e5ab3b54cc320c05b79c9ee740
Alex Balastegui Gaya <alexbalasteguigaya@enti.cat>
2018-05-03 19:42:49 +0200

    la ola se mueve
    sin guillermo rendimos más y mejor

--- glframework/code/src/physics.cpp
+++ glframework/code/src/physics.cpp
@@ -1,13 +1,16 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
 #include <glm\common.hpp>
+#include <glm\geometric.hpp>
 #include <math.h>
+#include <iostream>
 
 bool show_test_window = false;
 const int widthParticles = 14;
 const int depthParticles = 18;
 
 #define NPARTICLES 252
+#define PI 3.141592654
 
 using v3 = glm::vec3;
 struct particle {
@@ -16,7 +19,8 @@ struct particle {
 };
 
 float arrayParticles[NPARTICLES * 3];
-particle arrayStructParticles[NPARTICLES];
+particle arrayStructParticles[widthParticles][depthParticles];
+particle initialData[widthParticles][depthParticles];
 
 namespace ClothMesh {
 	extern void setupClothMesh();
@@ -45,32 +49,51 @@ void GUI() {
 	}
 }
 
+void arrayStructToArray() {
+	for (int i = 0; i < NPARTICLES; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			switch (j) {
+			case 0:
+				arrayParticles[i * 3 + j] = arrayStructParticles[i%widthParticles][i / widthParticles].pos.x;
+				break;
+			case 1:
+				arrayParticles[i * 3 + j] = arrayStructParticles[i%widthParticles][i / widthParticles].pos.y;
+				break;
+			case 2:
+				arrayParticles[i * 3 + j] = arrayStructParticles[i%widthParticles][i / widthParticles].pos.z;
+				break;
+			}
+		}
+	}
+}
+
 void initialDarticles() {
-	float localZ = -5.5;
-	float XPosition = -3.5;
-	float YPosition = 9.5;
-	float ZPosition = -2.5;
+	float localZ = -1;
+	float XPosition = -5;
+	float YPosition = 3;
+	float ZPosition = -5;
+	float gridExtentX = 10.f/static_cast<float>(widthParticles-1);
+	float gridExtentZ = 10.f/static_cast<float>(depthParticles-1);
 
 	for (int i = 0; i < NPARTICLES; ++i) {
 		for (int j = 0; j < 3; ++j) {
 			switch (j) {
 			case 0:
-				arrayParticles[i * 3 + j] = XPosition + (i % 14);
-				arrayStructParticles[i].pos.x = XPosition + (i % 14);
+				initialData[i%widthParticles][i/widthParticles].pos.x=arrayStructParticles[i%widthParticles][i/widthParticles].pos.x = XPosition + (i % widthParticles)*gridExtentX;
 				if (i % 14 == 0)
 					++localZ;
 				break;
 			case 1:
-				arrayParticles[i * 3 + j] = YPosition;
-				arrayStructParticles[i].pos.y = YPosition;
+				initialData[i%widthParticles][i / widthParticles].pos.y = arrayStructParticles[i%widthParticles][i / widthParticles].pos.y = YPosition;
 				break;
 			case 2:
-				arrayParticles[i * 3 + j] = ZPosition + (localZ);
-				arrayStructParticles[i].pos.z = ZPosition + (localZ);
+				initialData[i%widthParticles][i / widthParticles].pos.z = arrayStructParticles[i%widthParticles][i / widthParticles].pos.z = ZPosition + (localZ)*gridExtentZ;
 				break;
 			}
 		}
 	}
+
+	arrayStructToArray();
 }
 
 void PhysicsInit() {
@@ -81,7 +104,29 @@ void PhysicsInit() {
 	initialDarticles();
 }
 
+static float currentTime = 0;
 void PhysicsUpdate(float dt) {
+	currentTime += dt;
+
+	float lambda = 1;
+	float kWave = 2 * 3.141592 / lambda;
+	float A = 0.25; //amplitud de la ona
+	v3 K = {1, 1, 0.4}; //direcció de la ona
+	float w = 4; //velocitat de la ona
+
+
+	for (int i = 0; i < NPARTICLES; ++i) {
+		arrayStructParticles[i%widthParticles][i / widthParticles].pos = initialData[i%widthParticles][i / widthParticles].pos;
+		//x
+		arrayStructParticles[i%widthParticles][i / widthParticles].pos = initialData[i%widthParticles][i / widthParticles].pos - 
+			(K / kWave)*A*sin(glm::dot(K, initialData[i%widthParticles][i / widthParticles].pos)-w*currentTime);
+
+		//y
+		
+		arrayStructParticles[i%widthParticles][i / widthParticles].pos.y += A * cos(glm::dot(K,initialData[i%widthParticles][i / widthParticles].pos) - w * currentTime);
+	}
+
+	arrayStructToArray();
 	ClothMesh::updateClothMesh(arrayParticles);
 	ClothMesh::drawClothMesh();
 }
